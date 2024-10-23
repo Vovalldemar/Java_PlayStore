d68a7bd355f9c298340080301fa376e127c7a233
Vovalldemar <vovalldemar@yandex.ru>
2024-10-23 11:30:14 +0300

    add class for description Toy

--- Toy.java
+++ Toy.java
@@ -1,3 +1,33 @@
-public class Toy {
-    
+public class Toy implements Comparable<Toy> {
+    private int id;
+    private String name;
+    private int weight;
+
+    public Toy(int id, String name, int weight) {
+        this.id = id;
+        this.name = name;
+        this.weight = weight;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    @Override
+    public int compareTo(Toy other) {
+        return Integer.compare(other.weight, this.weight); // приоритет
+    }
+
+    @Override
+    public String toString() {
+        return "Toy{id=" + id + ", name='" + name + "', weight=" + weight + '}';
+    }
 }
